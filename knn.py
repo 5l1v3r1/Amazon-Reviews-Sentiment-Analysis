33a306b565e66eca6a82ef94f44290061b696ff6
boranyldrm <boranyldrm21@gmail.com>
2019-04-05 23:33:02 +0300

    some fixes knn

--- knn.py
+++ knn.py
@@ -1,12 +1,11 @@
 import pandas as pd
+from sklearn import metrics
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.model_selection import train_test_split
 
-from sklearn.naive_bayes import GaussianNB
-
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 
 dataset = pd.read_csv('dataset/foods_clean.csv', sep=";", quotechar="|", encoding='ISO-8859-1',
                       names=['idx', 'productid', 'userid', 'helpfulness', 'rating', 'time', 'summary', 'text', 'cleantext'],
@@ -40,17 +39,18 @@ def run_knn():
 
     # creating the feature matrix
     matrix = CountVectorizer(max_features=1000)
-    X = matrix.fit_transform(data.values.astype('U')).toarray()
-    dataset.ix[:, 4] = dataset.ix[:, 4].apply(pd.to_numeric)
-    y = dataset.iloc[:, 4]
+    X = matrix.fit_transform(data.iloc[:, -1].astype('U')).toarray()
+    data.iloc[:, 4] = data.iloc[:, 4].apply(pd.to_numeric)
+    y_tmp = data.iloc[:, 4]
+    y = []
+    for idx, val in y_tmp.iteritems():
+        if val == 4.0 or val == 5.0:
+            y.append("positive")
+        elif val == 1.0 or val == 2.0 or val == 3.0:
+            y.append("negative")
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.33)
-    positive_under = class_positive.sample(class_negative.shape[0])
-    print(positive_under.shape[0])
-    print(class_negative.shape[0])
-    df_test_under = pd.concat([positive_under, class_negative], axis=0)
-    print(df_test_under.shape[0])
-    return df_test_under
+    # split train and test data
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     clf = KNeighborsClassifier()
     clf.fit(X_train, y_train)
