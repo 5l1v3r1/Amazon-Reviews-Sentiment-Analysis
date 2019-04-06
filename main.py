e2b3279415c39cb50a937c9e3c08ecfe8867b45c
boranyildirim <boranyldrm21@gmail.com>
2019-04-06 13:59:22 +0300

    multinomial naive bayes implemented

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from data import get_data_frame, split_train_test
 from knn import run_knn
-from naive_bayes import run_naive_bayes
+from naive_bayes import run_gaussianNB, run_multinomialNB
 
 if __name__ == "__main__":
     data = get_data_frame()
     X_train, X_test, y_train, y_test = split_train_test(data)
 
-    run_naive_bayes(X_train, X_test, y_train, y_test)
+    # run_gaussianNB(X_train, X_test, y_train, y_test)
+    # run_multinomialNB(X_train, X_test, y_train, y_test)
     # run_knn(X_train, X_test, y_train, y_test)
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,10 +1,10 @@
-from sklearn.naive_bayes import GaussianNB
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
 
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 
-def run_naive_bayes(X_train, X_test, y_train, y_test):
-
+def run_gaussianNB(X_train, X_test, y_train, y_test):
     # Naive Bayes
     classifier = GaussianNB()
     classifier.fit(X_train, y_train)
@@ -13,7 +13,22 @@ def run_naive_bayes(X_train, X_test, y_train, y_test):
     y_pred = classifier.predict(X_test)
 
     # Confusion matrix
-    print("NAIVE BAYES")
+    print("GAUSSIAN NAIVE BAYES")
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    print(accuracy_score(y_test, y_pred))
+
+
+def run_multinomialNB(X_train, X_test, y_train, y_test):
+    tf_transformer = TfidfTransformer().fit_transform(X_train)
+
+    classifier = MultinomialNB()
+    classifier.fit(tf_transformer, y_train)
+
+    y_pred = classifier.predict(X_test)
+
+    # Confusion matrix
+    print("MULTINOMIAL NAIVE BAYES")
     print(confusion_matrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
     print(accuracy_score(y_test, y_pred))
