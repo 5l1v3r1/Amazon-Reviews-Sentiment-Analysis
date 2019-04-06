2b9cecdf9626163f708c218309f2fe8c53c2c8d7
boranyildirim <boranyldrm21@gmail.com>
2019-04-06 16:01:13 +0300

    linear regression and bernoulliNB are added to main

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from data import get_data_frame, split_train_test
 from knn import run_knn
-from naive_bayes import run_gaussianNB, run_multinomialNB
+from linear_regression import run_linear_regression
+from naive_bayes import run_gaussianNB, run_multinomialNB, run_bernoulliNB
 
 if __name__ == "__main__":
     data = get_data_frame()
     X_train, X_test, y_train, y_test = split_train_test(data)
 
+    # Uncomment the algorithms to run as you wish
+    # be careful with knn it is too slow.
+
     # run_gaussianNB(X_train, X_test, y_train, y_test)
     # run_multinomialNB(X_train, X_test, y_train, y_test)
+    # run_bernoulliNB(X_train, X_test, y_train, y_test)
     # run_knn(X_train, X_test, y_train, y_test)
+    # run_linear_regression(X_train, X_test, y_train, y_test)
