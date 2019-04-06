f36a9209e05083a689b7c4454b42226811a04854
boranyildirim <boranyldrm21@gmail.com>
2019-04-06 13:34:14 +0300

    main and dataread removed from computation and combined on main.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from sklearn.feature_extraction.text import CountVectorizer
 
 from sklearn.model_selection import train_test_split
@@ -8,33 +6,8 @@ from sklearn.naive_bayes import GaussianNB
 
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
-dataset = pd.read_csv('dataset/foods_clean.csv', sep=";", quotechar="|", encoding='ISO-8859-1',
-                      names=['idx', 'productid', 'userid', 'helpfulness', 'rating', 'time', 'summary', 'text', 'cleantext'],
-                      header=None)
 
-
-def balance():
-    print(dataset[dataset['rating'] == 5.0].shape[0])
-    print(dataset[dataset['rating'] == 4.0].shape[0])
-    class_positive = dataset[dataset['rating'] == 5.0]
-    class_positive = class_positive.append(dataset[dataset['rating'] == 4.0], ignore_index=True)
-    print(class_positive.shape[0])
-
-    class_negative = dataset[dataset['rating'] == 1.0]
-    class_negative = class_negative.append(dataset[dataset['rating'] == 2.0], ignore_index=True)
-    class_negative = class_negative.append(dataset[dataset['rating'] == 3.0], ignore_index=True)
-
-    positive_under = class_positive.sample(class_negative.shape[0])
-    print(positive_under.shape[0])
-    print(class_negative.shape[0])
-    df_test_under = pd.concat([positive_under, class_negative], axis=0)
-    print(df_test_under.shape[0])
-    return df_test_under
-
-
-def run_naive_bayes():
-
-    data = balance()
+def run_naive_bayes(data):
 
     # creating the feature matrix
     matrix = CountVectorizer(max_features=1000)
@@ -58,24 +31,7 @@ def run_naive_bayes():
     y_pred = classifier.predict(X_test)
 
     # Confusion matrix
-
-    cm = confusion_matrix(y_test, y_pred)
-    print(cm)
-    cr = classification_report(y_test, y_pred)
-    print(cr)
-
-    accuracy = accuracy_score(y_test, y_pred)
-    print(accuracy)
-
-
-if __name__ == '__main__':
-    p = 0
-    n = 0
-    for idx, val in dataset['rating'].iteritems():
-        if val == 5.0 or val == 4.0:
-            p += 1
-        else:
-            n += 1
-    print(p)
-    print(n)
-    run_naive_bayes()
+    print("NAIVE BAYES")
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    print(accuracy_score(y_test, y_pred))
