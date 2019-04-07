74e8b9dd3636aca55f44929e4f97997ae29befb1
boranyldrm <boranyldrm21@gmail.com>
2019-04-07 10:27:27 +0300

    knn number of neighbours decreased to 2 to make it faster

--- knn.py
+++ knn.py
@@ -7,7 +7,7 @@ def run_knn(X_train, X_test, y_train, y_test):
 
     # https://www.youtube.com/watch?v=s-9Qqpv2hTY
 
-    clf = KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
+    clf = KNeighborsClassifier(n_neighbors=2, n_jobs=-1)
     clf.fit(X_train, y_train)
 
     y_pred = clf.predict(X_test)
