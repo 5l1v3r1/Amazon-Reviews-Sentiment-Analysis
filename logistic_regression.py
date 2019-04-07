8cb46c95e118cc5909899ee0f7faa18d8fd9f366
boranyldrm <boranyldrm21@gmail.com>
2019-04-07 10:28:31 +0300

    regression class made binary. prediction values are rounded.

--- logistic_regression.py
+++ logistic_regression.py
@@ -10,12 +10,13 @@ def run_logistic_regression(X_train, X_test, y_train, y_test):
 
     # see https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
     # to use different solvers and classes
-    regr = LogisticRegression(solver='saga', multi_class='multinomial', n_jobs=-1)
+    regr = LogisticRegression(solver='saga', multi_class='ovr', n_jobs=-1)
     regr.fit(X_train, y_train_f)
 
     y_pred = regr.predict(X_test)
+    y_pred = y_pred.round().astype(int)
 
     print("LOGISTIC REGRESSION")
-    print(confusion_matrix(y_test_f, y_pred.round()))
-    print(classification_report(y_test_f, y_pred.round()))
-    print(accuracy_score(y_test_f, y_pred.round()))
+    print(confusion_matrix(y_test_f, y_pred))
+    print(classification_report(y_test_f, y_pred))
+    print(accuracy_score(y_test_f, y_pred))
