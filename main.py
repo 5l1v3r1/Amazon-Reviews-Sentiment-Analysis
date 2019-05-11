400891de6462fa1159b54cd42a8fef9e9056a0d8
boranyildirim <boranyldrm21@gmail.com>
2019-05-11 15:31:31 +0300

    updated main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import multiprocessing
 
+from ada_boost import run_ada_boost
 from data import get_data_frame, split_train_test
+from decision_tree import run_decision_tree
 from knn import run_knn
 from linear_regression import run_linear_regression
 from logistic_regression import run_logistic_regression
 from naive_bayes import run_gaussianNB, run_multinomialNB, run_bernoulliNB
+from neural_network import run_neural_network
+from random_forest import run_random_forest
+from svm import run_svm
 
 if __name__ == "__main__":
     data = get_data_frame()
@@ -16,6 +21,11 @@ if __name__ == "__main__":
     #run_gaussianNB(X_train, X_test, y_train, y_test)
     #run_multinomialNB(X_train, X_test, y_train, y_test)
     #run_bernoulliNB(X_train, X_test, y_train, y_test)
-    # run_knn(X_train, X_test, y_train, y_test)
-    # run_linear_regression(X_train, X_test, y_train, y_test)
-    run_logistic_regression(X_train, X_test, y_train, y_test)
+    #run_knn(X_train, X_test, y_train, y_test)
+    #run_linear_regression(X_train, X_test, y_train, y_test)
+    #run_logistic_regression(X_train, X_test, y_train, y_test)
+    #run_svm(X_train, X_test, y_train, y_test)
+    #run_random_forest(X_train, X_test, y_train, y_test)
+    #run_decision_tree(X_train, X_test, y_train, y_test)
+    #run_neural_network(X_train, X_test, y_train, y_test)
+    run_ada_boost(X_train, X_test, y_train, y_test)
