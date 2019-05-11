903155adf97529053bbec3e2a691d0f2d908b82e
boranyildirim <boranyldrm21@gmail.com>
2019-05-11 15:30:30 +0300

    linear regression errors etc.

--- linear_regression.py
+++ linear_regression.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, mean_squared_error, r2_score
 from data import y_to_float
+import matplotlib.pyplot as plt
 
 
 def run_linear_regression(X_train, X_test, y_train, y_test):
@@ -17,3 +18,11 @@ def run_linear_regression(X_train, X_test, y_train, y_test):
     print(confusion_matrix(y_test_f, y_pred))
     print(classification_report(y_test_f, y_pred))
     print(accuracy_score(y_test_f, y_pred))
+
+    # The coefficients
+    print('Coefficients: \n', regr.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f"
+          % mean_squared_error(y_test_f, y_pred))
+    # Explained variance score: 1 is perfect prediction
+    print('Variance score: %.2f' % r2_score(y_test_f, y_pred))
