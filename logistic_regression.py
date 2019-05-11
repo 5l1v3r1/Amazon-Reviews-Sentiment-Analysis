276b348938d3c2df25dd623a06c1810ecb9b58de
boranyildirim <boranyldrm21@gmail.com>
2019-05-11 15:31:52 +0300

    logistic regression solver change

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,7 +2,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 from data import y_to_float
-from naive_bayes import plot
 
 
 def run_logistic_regression(X_train, X_test, y_train, y_test):
@@ -11,17 +10,13 @@ def run_logistic_regression(X_train, X_test, y_train, y_test):
 
     # see https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
     # to use different solvers and classes
-    regr = LogisticRegression(solver='saga', multi_class='ovr', n_jobs=-1)
+    regr = LogisticRegression(solver='liblinear', multi_class='ovr', n_jobs=-1, max_iter=200)
     regr.fit(X_train, y_train_f)
 
     y_pred = regr.predict(X_test)
     y_pred = y_pred.round().astype(int)
 
-    y_score = regr.decision_function(X_test)
-
     print("LOGISTIC REGRESSION")
     print(confusion_matrix(y_test_f, y_pred))
     print(classification_report(y_test_f, y_pred))
     print(accuracy_score(y_test_f, y_pred))
-
-    plot(y_test_f, y_score)
